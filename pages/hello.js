c6281df03f6ce412da403bba2507e2ae6027e7cd
webonly-designs <webonly.designs@gmail.com>
2023-06-20 18:39:44 -0500

    hel

--- pages/hello.js
+++ pages/hello.js
@@ -32,20 +32,6 @@ export async function getServerSideProps({query}) {
     }
   }
 
-  export async function getServerSideProps({query}) {
-    const {id} = query
-    const url = `https://bayut.p.rapidapi.com/properties/detail?externalID=${id}`
-    const resProperty = await fetch(url, {
-      headers: {
-        'X-RapidAPI-Key': 'ecc720d6c7mshc52a79cc34f9104p15f53djsnf06cb96082bf',
-        'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-     }})
-     const propertyData = await resProperty.json()
-     return {
-      props: {
-        propertyData: propertyData
-      }
-     }
-  }
+  
 
 export default hello
